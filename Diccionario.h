9f8dc09292983f5e7811ecf27561a7dd3693e6be
Santiago TIssoni <santi.tissoni@gmail.com>
2021-01-10 17:16:48 -0300

    Update Diccionario.h

--- Diccionario.h
+++ Diccionario.h
@@ -12,10 +12,15 @@ private:
     Nodo * raiz;
     int cantidad_nodos;
 public:
+    // Constructor.
     Diccionario();
+    // Devuelve el numero de entradas en el diccionario.
     int largo();
+    // Se inserta una entrada en el diccionario. La entrada esta compuesta por un par key: string y value: puntero a Personaje.
     void insertar_dato(Key key, Value value);
+    // Se le pasa una key, y devuelve un puntero a Personaje si lo encuentra con la key proporcionada.
     Value buscar(Key key);
+    // Recorre el diccionario en busca de la key, devuelve true si encuentra la key, sino devuelve false.
     bool esta_key(Key key);
 };
 

e4f6e6717ca772cf6f85156767e0c7db166180ec
Santiago TIssoni <santi.tissoni@gmail.com>
2021-01-13 10:59:43 -0300

    Cmake para Clion

---
+++ cmake-build-debug/CMakeFiles/batalla_elementos.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/santi/GitHub/batalla-elementos/Agua.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Agua.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Aire.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Aire.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Diccionario.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Diccionario.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Fuego.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Fuego.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Nodo.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Nodo.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Personaje.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Personaje.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/Tierra.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/Tierra.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/funciones.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/funciones.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/jugador.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/jugador.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/main.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/main.cpp.obj"
+  "C:/Users/santi/GitHub/batalla-elementos/menu.cpp" "C:/Users/santi/GitHub/batalla-elementos/cmake-build-debug/CMakeFiles/batalla_elementos.dir/menu.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

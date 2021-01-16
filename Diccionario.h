4fc53b706f419971d7e9f8c858987f0f167d6f0a
MauricioDavico <77125998+MauricioDavico@users.noreply.github.com>
2021-01-16 15:53:51 -0300

    Update Diccionario.h

--- Diccionario.h
+++ Diccionario.h
@@ -17,13 +17,13 @@ public:
     Diccionario();
     
     // Agrega un par clave:valor al diccionario. Incrementa cantidad_nodos en uno.
-    void alta(Key key, Value value);
+    void alta(Clave clave, Valor valor);
     
     // Elimina del diccionario el nodo que contiene la clave pasada por parametro. Decrementa cantidad_nodos en uno.
-    void baja(Key key);
+    void baja(Clave clave);
     
     // Se le pasa una clave, y devuelve un puntero a Personaje si lo encuentra con la clave proporcionada.
-    Value buscar(Key key);
+    Value buscar(Clave clave);
     
     // Encuentra el minimo valor en el diccionario.
     void buscar_min(Nodo* raiz);
@@ -32,19 +32,19 @@ public:
     bool es_raiz();
     
     // Recorre el diccionario en busca de la clave, devuelve true si la encuentra, sino devuelve false.
-    bool esta_key(Key key);
+    bool esta_key(Clave clave);
     
     // Devuelve true si el diccionario esta vacio, false de lo contrario.
     bool esta_vacio();
     
     // Se inserta una entrada en el diccionario. La entrada esta compuesta por un par clave: string y valor: puntero a Personaje.
-    void insertar_dato(Key key, Value value);
+    void insertar_dato(Clave clave, Valor valor);
     
     // Devuelve el numero de entradas en el diccionario.
     int largo();
     
     // Encuentra el sucesor del nodo que contiene la clave pasada por parametro
-    void sucesor(Nodo* raiz, Nodo*& suc, Key clave);
+    void sucesor(Nodo* raiz, Nodo*& suc, Clave clave);
 
 
     // Destructor

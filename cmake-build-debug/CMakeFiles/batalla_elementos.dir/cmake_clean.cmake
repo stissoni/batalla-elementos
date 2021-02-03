10415b55e91ef828afed1467166aac8770de4e7f
Santiago TIssoni <santi.tissoni@gmail.com>
2021-02-03 19:46:46 -0300

    Test clion

---
+++ cmake-build-debug/CMakeFiles/batalla_elementos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/batalla_elementos.dir/main.cpp.obj"
+  "batalla_elementos.exe"
+  "batalla_elementos.exe.manifest"
+  "batalla_elementos.pdb"
+  "libbatalla_elementos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/batalla_elementos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
